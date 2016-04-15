b9aeb88ea1c48e927b3b42d4f648b58af510ad1e
cindy4580 <cindyleehere@hotmail.com>
2016-04-15 10:36:57 -0400

    Bug fixed in pdf

--- @vmmdistribution/pdf.m
+++ @vmmdistribution/pdf.m
@@ -1,11 +1,10 @@
-function y = pdf(X,obj,num)
+function y = pdf(X,obj)
 %VMMDISTRIBUTION/PDF PDF for a von Mises mixture distribution 
-%   Y = PDF(X,OBJ,NUM) returns Y, a vector of length N containing the
+%   Y = PDF(X,OBJ) returns Y, a vector of length N containing the
 %   probability density function (p.d.f.) for the von Mises distributuion
 %   OBJ, evalueated at the N-by-2 data matrix X. Rows of X correspond to
 %   obseration data points while columns correspond to variables. Y(I) is
-%   the p.d.f. value of point I. NUM is the cutoff for infinite summation
-%   of Bessel functions in the normalization constant
+%   the p.d.f. value of point I. 
 %
 %   See also VMMDISTRIBUTION, VMMDISTRIBUTION/CDF
 %
@@ -13,18 +12,14 @@ function y = pdf(X,obj,num)
 %   Copyright: Xindi Li (xindi.li@stonybrook.edu)
 
 % Check for valid input
-if nargin ~= 3
-    error('TooFewInputs');
+if nargin > 2
+    error('TooManyInputs');
 end
 checkdata(X,obj);
-
-% Model selection
-ModelNames  = {'Sine','Cosine'};
-CorType     = find(strncmpi(obj.CorType,ModelNames,length(obj.CorType)));
-
 % Calculation
-log_lh      = wdensity(X,obj.Mu,obj.Kappa,obj.Lambda,obj.Pcomponents, ...
-                         CorType,num);
-y = sum(exp(log_lh),2);
+num = 100;
+log_lh = wdensity(X,obj.Mu,obj.Kappa,obj.Lambda,obj.Pcomponents,...
+                    obj.CorType,num);
+y      = sum(exp(log_lh),2);
 end
 
